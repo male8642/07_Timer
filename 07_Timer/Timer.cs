e623f19c10e9dcd8177fd7aaed413b1267711a99
Malene <Male8642@edu.eal.dk>
2016-09-20 12:12:04 +0200

    okay?

--- 07_Timer/Timer.cs
+++ 07_Timer/Timer.cs
@@ -21,23 +21,19 @@ namespace _07_Timer
         public override string ToString()
         {
             int minutes = seconds / 60;
+            seconds = seconds - 60 * minutes;
             int hours = minutes / 60;
+            minutes = minutes - 60 * hours;
 
-            if (seconds > 60)
+      
+           if (seconds < 10)
             {
-                minutes = seconds / 60;
-                seconds = seconds - 60 * minutes;
-                return "00:" + "0" + minutes + ":" + "0" + seconds;
+                return "0" + hours + ":" + "0" + minutes + ":" + "0" + seconds;
             }
 
-            else if (minutes > 60)
-             {
-                 hours = minutes / 60;
-                minutes = minutes - 60 * hours;
-               
-                return "0" + hours + ":" + "0" + minutes + ":" + seconds;
-             }
-            return Seconds + "";
+            
+            return "0" + hours + ":" + "0" + minutes + ":" + seconds;
+           
 
             }
 
